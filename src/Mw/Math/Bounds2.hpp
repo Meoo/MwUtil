68b15ae05e1d3a664f98675ac0bab34ed5ed02d4
Bastien Brunnenstein <bastien@brunnenstein.fr>
2013-09-24 18:18:45 +0200

    Comments on Bounds2, and intersection function

--- src/Mw/Math/Bounds2.hpp
+++ src/Mw/Math/Bounds2.hpp
@@ -40,9 +40,23 @@ public:
     {}
 
     /**
+     * Constructor.
+     *
+     * Create the bounds from two points.
+     * If the two points are equals, the resulting bounds will be empty.
+     *
+     * @param first First point.
+     * @param second Second point.
+     */
+    Bounds2(const Vector & first, const Vector & second)
+    {
+        set(first, second);
+    }
+
+    /**
      * Copy constructor.
      *
-     * @param vec Vector to copy.
+     * @param bounds Bounds to copy.
      */
     template <typename U>
     Bounds2(const Bounds2<U> & bounds)
@@ -55,16 +69,34 @@ public:
             || _upper_limit.getY() <= _lower_limit.getY();
     }
 
+    /**
+     * Get the upper limit of this bounds.
+     *
+     * @return Upper limit of this bounds.
+     */
     const Vector & getUpperLimit() const
     {
         return _upper_limit;
     }
 
+    /**
+     * Get the lower limit of this bounds.
+     *
+     * @return Lower limit of this bounds.
+     */
     const Vector & getLowerLimit() const
     {
         return _lower_limit;
     }
 
+    /**
+     * Reset the bounds using two points.
+     *
+     * If the two points are equals, the resulting bounds will be empty.
+     *
+     * @param first First point.
+     * @param second Second point.
+     */
     void set(const Vector & first, const Vector & second)
     {
         if (first.getX() > second.getX())
@@ -90,18 +122,37 @@ public:
         }
     }
 
+    /**
+     * Change the upper limit of the bounds.
+     *
+     * If the value is inferior to the lower limit, the bounds will be empty.
+     *
+     * @param upper_limit New upper limit of the bounds.
+     */
     void setUpperLimit(const Vector & upper_limit)
     {
         _upper_limit = upper_limit;
     }
 
+    /**
+     * Change the lower limit of the bounds.
+     *
+     * If the value is superior to the upper limit, the bounds will be empty.
+     *
+     * @param lower_limit New lower limit of the bounds.
+     */
     void setLowerLimit(const Vector & lower_limit)
     {
         _lower_limit = lower_limit;
     }
 
+    /**
+     * Expand the bounds to include a point.
+     *
+     * @param vec Point to include in this bounds.
+     */
     template <typename U>
-    void join(const Vector2<U> & vec)
+    void include(const Vector & vec)
     {
         if (vec.getX() > _upper_limit.getX())
             _upper_limit.setX(vec.getX());
@@ -116,15 +167,52 @@ public:
             _lower_limit.setY(vec.getY());
     }
 
+    /**
+     * Expand the bounds to include another bounds.
+     *
+     * @param bounds Other bounds to include in this bounds.
+     */
     template <typename U>
-    void join(const Bounds2<U> & bounds)
+    void include(const Bounds2<U> & bounds)
     {
         join(bounds.getUpperLimit());
         join(bounds.getLowerLimit());
     }
 
+    /**
+     * Reduce the size of this bounds to fit into given bounds.
+     *
+     * If the two bounds does not intersect, this object becomes empty.
+     *
+     * @param bounds Other bounds for this bounds to be included into.
+     */
     template <typename U>
-    void isInside(const Vector2<U> & vec) const
+    void intersect(const Bounds2<U> & bounds)
+    {
+        const Vector2<U> & upper = bounds.getUpperLimit();
+        const Vector2<U> & lower = bounds.getLowerLimit();
+
+        if (upper.getX() < _upper_limit.getX())
+            _upper_limit.setX(upper.getX());
+
+        if (upper.getY() < _upper_limit.getY())
+            _upper_limit.setY(upper.getY());
+
+        if (lower.getX() > _lower_limit.getX())
+            _lower_limit.setX(lower.getX());
+
+        if (lower.getY() > _lower_limit.getY())
+            _lower_limit.setY(lower.getY());
+    }
+
+    /**
+     * Check if a point is inside this bounds.
+     *
+     * @param vec Point to check.
+     * @return @c true if the point is inside the bounds.
+     */
+    template <typename U>
+    bool isInside(const Vector & vec) const
     {
         return vec.getX() <= _upper_limit.getX()
             && vec.getY() <= _upper_limit.getY()
@@ -132,8 +220,14 @@ public:
             && vec.getY() >= _lower_limit.getY();
     }
 
+    /**
+     * Check if another bounds is included inside this bounds.
+     *
+     * @param bounds Other bounds to check.
+     * @return @c true if the bounds are included in this bounds.
+     */
     template <typename U>
-    void isInside(const Bounds2<U> & bounds) const
+    bool isInside(const Bounds2<U> & bounds) const
     {
         return bounds.getUpperLimit().getX() <= _upper_limit.getX()
             && bounds.getUpperLimit().getY() <= _upper_limit.getY()
@@ -146,7 +240,8 @@ public:
 
 
 /**
- * @brief Stream insertion operator overload.
+ * Stream insertion operator overload.
+ *
  * @param ostr Output stream.
  * @param bnd Bounds to insert into the stream.
  * @return @c ostr Output stream.
