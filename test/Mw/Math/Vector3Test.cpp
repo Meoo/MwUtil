479fc09bdb55487db9bfdb81d080c70e9cdf9ae4
Bastien Brunnenstein <bastien@brunnenstein.fr>
2013-09-28 17:59:57 +0200

    Commented out the Vector3Test file, not implemented yet

--- test/Mw/Math/Vector3Test.cpp
+++ test/Mw/Math/Vector3Test.cpp
@@ -13,7 +13,7 @@
 #define EPSILON std::numeric_limits<T>::epsilon() * 100
 
 typedef boost::mpl::list<float, double> test_types;
-
+/* TODO Vector3 unit test
 BOOST_AUTO_TEST_SUITE(Math)
 BOOST_AUTO_TEST_SUITE(Vector3)
 
@@ -49,3 +49,4 @@ BOOST_AUTO_TEST_CASE_TEMPLATE(Normals, T, test_types)
 
 BOOST_AUTO_TEST_SUITE_END()
 BOOST_AUTO_TEST_SUITE_END()
+*/
